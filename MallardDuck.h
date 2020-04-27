325a072a043aab5202e9de1cc43c58c38bf92a49
Artem4ikPE <64414765+Artem4ikPE@users.noreply.github.com>
2020-04-27 16:23:29 +0300

    Сделал MalardDuck

--- MallardDuck.h
+++ MallardDuck.h
@@ -1,1 +1,7 @@
 #pragma once
+#include "duck.h" 
+class MallardDuck :
+	public Duck
+{
+public: MallardDuck(void); ~MallardDuck(void); void display();
+};
---
+++ Source6.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "MallardDuck.h"
+MallardDuck::MallardDuck(void)
+{
+}
+MallardDuck::~MallardDuck(void)
+{
+}
+void MallardDuck::display()
+{
+	cout << "Mallard" << endl;
+}
